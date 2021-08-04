edce4c79907dbfb806d9b728ac29dae67c72cb9d
Owen Tuleja <owen@arborbridge.com>
2021-08-04 15:19:18 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <div>yo {process.env.REACT_APP_MY_VAR}</div>
       <div>yo{process.env.NODE_ENV}</div>
+      {/* <div>yo{process.env.REACT_APP_DSN}</div> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+Sentry.init({
+  dsn: process.env.REACT_APP_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+console.log("here");
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
